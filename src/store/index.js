9364e287f2e89791241ea5fcf60ec7340d393f68
GuiPoZhou <1207336828@qq.com>
2024-05-29 09:32:27 +0800

    init

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,7 @@
 
 import Vuex from 'vuex'
 import app from './modules/app'
-import system from './system/index'
-import process from './modules/process'
-import standardManagement from './modules/standardManagement'
-import masterBanUi from "@/store/modules/masterBanUi";
-import BeiYuan from "./modules/BeiYuan";
-import equipment from './modules/equipment';
-import examTest from "@/store/modules/examTest";
-import jobRotationRecord from "@/store/modules/jobRotationRecord.js";
+
 
 export default new Vuex.Store({
   state: {
@@ -18,14 +11,6 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
-    equipment,
-    system,
-    process,
-    standardManagement,
     app,
-    BeiYuan,
-    masterBanUi,
-    examTest,
-    jobRotationRecord,
   }
 })
